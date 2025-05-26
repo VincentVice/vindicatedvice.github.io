2be304cf9b88fabcc1ad095e10dd9a1fc5f94526
VincentVice <gungyo420@gmail.com>
2025-05-26 19:21:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const player = {
   name: "Hero",
   hp: 100,
-  attack: 10
+  attack: 10,
 };
 
 let enemyLevel = 1;
-let enemy = createEnemy(enemyLevel);
 
 function createEnemy(level) {
   return {
     name: "Enemy Lv" + level,
     hp: 50 + level * 10,
-    attack: 5 + level * 2
+    attack: 5 + level * 2,
   };
 }
 
+let enemy = createEnemy(enemyLevel);
+
 function updateStats() {
-  document.getElementById("playerStats").textContent = player.name + " - HP: " + player.hp;
-  document.getElementById("enemyStats").textContent = enemy.name + " - HP: " + enemy.hp;
+  document.getElementById("playerStats").textContent =
+    player.name + " - HP: " + player.hp;
+  document.getElementById("enemyStats").textContent =
+    enemy.name + " - HP: " + enemy.hp;
 }
 
 function log(message) {
@@ -25,26 +28,31 @@ function log(message) {
   logDiv.innerHTML = message + "<br>" + logDiv.innerHTML;
 }
 
-document.getElementById("attackBtn").addEventListener("click", function () {
+document.getElementById("attackBtn").addEventListener("click", () => {
   if (player.hp <= 0) {
-    log("You're already defeated!");
+    log("You're defeated! Refresh the page to try again.");
     return;
   }
 
   enemy.hp -= player.attack;
-  log("You hit " + enemy.name + " for " + player.attack + " damage.");
+  log(You hit ${enemy.name} for ${player.attack} damage.);
 
   if (enemy.hp <= 0) {
-    log(enemy.name + " is defeated!");
+    log(${enemy.name} is defeated!);
     enemyLevel++;
     enemy = createEnemy(enemyLevel);
-    log("A new " + enemy.name + " appears!");
+    log(A new ${enemy.name} appears!);
   } else {
     player.hp -= enemy.attack;
-    log(enemy.name + " hits you for " + enemy.attack + " damage.");
+    log(${enemy.name} hits you for ${enemy.attack} damage.);
   }
 
   if (player.hp <= 0) {
     log("You have been defeated.");
     document.getElementById("attackBtn").disabled = true;
   }
+
+  updateStats();
+});
+
+updateStats();
