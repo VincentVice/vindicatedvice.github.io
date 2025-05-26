43f4fa04a7d62bcc6ef231aa734a75c8098226fb
VincentVice <gungyo420@gmail.com>
2025-05-26 19:15:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const player = {
   name: "Hero",
   hp: 100,
@@ -10,49 +9,42 @@ let enemy = createEnemy(enemyLevel);
 
 function createEnemy(level) {
   return {
-    name: Enemy Lv${level},
+    name: "Enemy Lv" + level,
     hp: 50 + level * 10,
     attack: 5 + level * 2
   };
 }
 
 function updateStats() {
-  document.getElementById("playerStats").textContent =
-    ${player.name} - HP: ${player.hp};
-  document.getElementById("enemyStats").textContent =
-    ${enemy.name} - HP: ${enemy.hp};
+  document.getElementById("playerStats").textContent = player.name + " - HP: " + player.hp;
+  document.getElementById("enemyStats").textContent = enemy.name + " - HP: " + enemy.hp;
 }
 
-function log(msg) {
+function log(message) {
   const logDiv = document.getElementById("log");
-  logDiv.innerHTML = msg + "<br>" + logDiv.innerHTML;
+  logDiv.innerHTML = message + "<br>" + logDiv.innerHTML;
 }
 
-document.getElementById("attackBtn").addEventListener("click", () => {
+document.getElementById("attackBtn").addEventListener("click", function () {
   if (player.hp <= 0) {
     log("You're already defeated!");
     return;
   }
 
   enemy.hp -= player.attack;
-  log(You hit ${enemy.name} for ${player.attack} damage.);
+  log("You hit " + enemy.name + " for " + player.attack + " damage.");
 
   if (enemy.hp <= 0) {
-    log(${enemy.name} is defeated!);
+    log(enemy.name + " is defeated!");
     enemyLevel++;
     enemy = createEnemy(enemyLevel);
-    log(A new ${enemy.name} appears!);
+    log("A new " + enemy.name + " appears!");
   } else {
     player.hp -= enemy.attack;
-    log(${enemy.name} hits you for ${enemy.attack} damage.);
+    log(enemy.name + " hits you for " + enemy.attack + " damage.");
   }
 
   if (player.hp <= 0) {
     log("You have been defeated.");
     document.getElementById("attackBtn").disabled = true;
   }
-
-  updateStats();
-});
-
-updateStats();
